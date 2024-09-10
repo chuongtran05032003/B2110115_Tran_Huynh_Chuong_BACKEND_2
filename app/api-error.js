5e713b547e99851585d21e80dbf37809eae4f4a4
chuongtran <chuongtran05032003@gmail.com>
2024-09-10 20:32:59 +0700

    Cai dat xy ly loi

--- app/api-error.js
+++ app/api-error.js
@@ -1,5 +1,5 @@
 class ApiError extends Error {
-    constructor(stateCode, message){
+    constructor(stateCode, message) {
         super();
         this.stateCode = stateCode;
         this.message = message;
